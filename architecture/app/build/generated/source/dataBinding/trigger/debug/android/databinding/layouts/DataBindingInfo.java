173485155c5cc505539ffc7bcb9a77da707f7ba0
Unknown <joe@fastcomm.com>
2018-05-25 09:47:09 +0200

    [Added] Architecture Components Tutorial

---
+++ architecture/app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="239971fa-32df-40c3-9190-a7b3bb24eb6b")
+public class DataBindingInfo {}
